5aab3a3140af682eb3aca37646992bc1328c83ef
AstridGlauser234 <91710814+AstridGlauser234@users.noreply.github.com>
2021-10-01 10:06:20 -0600

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -1,35 +1,38 @@
 
 
 class usuario():
+
+
+class usuario():  # creación de la clase
     
     numUsuarios = 0
 
-    def __init__(self,nombre,contra):
-        self.nombre = nombre
+    def __init__(self,nombre,contra):# self, tratamos un objet
+        self.nombre = nombre #asignación del nombre, es como this en java
         self.contra = contra
 
         self.conectado = False
-        self.intentos = 5
+        self.intentos = 5 # se le brindan al usuario n intentos
 
-        usuario.numUsuarios+=1
+        usuario.numUsuarios+=1 # se suma un número de usuario
     
-    def conectar(self,contrasenia=None):
+    def conectar(self,contrasenia=None): # Si el usuario no llegará a ingresar una contraseña
         if contrasenia==None:
             myContra = input("Ingrese su contraseña: ")
         else:
-            myContra=contrasenia
+            myContra=contrasenia 
         if myContra==self.contra:
       
-            self.conectado = True
-            return True
+            self.conectado = True # valida que la condición sea verdadwera 
+            return True # retorna un booleano
         else:
-            self.intentos-=1
+            self.intentos-=1 # le quita un intetno
             if self.intentos > 0:
             
                 if contrasenia!=None:
-                    return False
+                    return False # retorna booleano
              
-                self.conectar()
+                self.conectar() # función
             
               
     
